cf3ee8c776b67b0efee8c9469b79a7329e285432
Putranugroho14 <putranugroho61@gmail.com>
2025-10-28 14:56:45 +0700

    Setup express.js

---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const app = express();
+const db = require('./models');
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(PORT, async () => {
+  console.log(`Server is running on http://localhost:${PORT}`); 
+});
+
+db.sequelize.sync().then((result) => {
+    app.listen(3000, () => {
+        console.log('Server Started');
+    })
+})
+  .catch((err) => {
+    console.log(err);
+});
+
+app.post('/komik', async (req, res) => {
+    const data = req.body;
+    try {
+        const komik = await db.Komik.create(data);
+        res.send(komik);
+    } catch (error) {
+        res.status(500).send({ message: error.message });
+    }
+});
+
+app.get('/komik', async (req, res) => {
+    try {
+        const komik = await db.Komik.findAll();
+        res.send(komik);
+    } catch (error) {
+        res.status(500).send({ message: error.message });
+    }
+});
+app.put('/komik/:id', async (req, res) => {
+    const id = req.params.id;
+    const data = req.body;
+
+    try {
+        const komik = await db.Komik.findByPk(id);
+        if (!komik) {
+            return res.status(404).send({ message: 'Komik not found' });
+        }
+        await komik.update(data);
+        res.send({ message: 'Komik berhasil di update', komik });
+    } catch (error) {
+        res.status(500).send({ message: error.message });
+    }
+});
+
+app.delete('/komik/:id', async (req, res) => {
+    const id = req.params.id;
+    try {
+        const komik = await db.Komik.findByPk(id);
+        if (!komik) {
+            return res.status(404).send({ message: 'Komik not found' });
+        }
+        await komik.destroy();
+        res.send({ message: "Komik berhasil dihapus" });
+    } catch (error) {
+        res.status(500).send({ message: error.message });
+    }
+});
